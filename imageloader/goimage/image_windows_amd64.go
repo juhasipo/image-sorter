d13035d7589f8abacf21ddbc6a79235f5cdac0a4
Juha Siponen <juha.siponen@vincit.fi>
2020-07-21 16:18:06 +0300

    Make Windows version to use libjpeg to boost performance

--- imageloader/goimage/image_windows_amd64.go
+++ imageloader/goimage/image_windows_amd64.go
@@ -1,24 +1,35 @@
 package goimage
 
 import (
+	"github.com/pixiv/go-libjpeg/jpeg"
 	"image"
-	"image/jpeg"
-	"log"
 	"os"
 	"vincit.fi/image-sorter/common"
 )
+var options = &jpeg.DecoderOptions{}
 
 func LoadImage(handle *common.Handle) (image.Image, error) {
-	log.Printf("Loading image %s", handle.GetId())
-	imageFile, err := os.Open(handle.GetPath())
-	if err != nil {
-		return nil, err
-	}
-	defer imageFile.Close()
+	file, err := os.Open(handle.GetPath())
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: Convert to RGB if necessary
-	return jpeg.Decode(imageFile)
+	imageFile, err := jpeg.Decode(file, options)
+	if err != nil {
+		return nil, err
+	}
+	return imageFile, err
+}
+
+func LoadImageScaled(handle *common.Handle, size common.Size) (image.Image, error) {
+	file, err := os.Open(handle.GetPath())
+	if err != nil {
+		return nil, err
+	}
+
+	imageFile, err := jpeg.Decode(file, &jpeg.DecoderOptions{ScaleTarget: image.Rect(0, 0, size.GetWidth(), size.GetHeight())})
+	if err != nil {
+		return nil, err
+	}
+	return imageFile, err
 }
