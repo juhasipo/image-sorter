f51e0e595bc7a47f8c6559397f911f62146edbe6
Juha Siponen <juha.siponen@vincit.fi>
2021-01-06 20:02:03 +0200

    Minimize binary size

--- script/build-linux.sh
+++ script/build-linux.sh
@@ -5,7 +5,7 @@ VERSION="$(cat VERSION)-$(git rev-parse --short HEAD)"
 sed -i "s/__IMAGE_SORTER_VERSION__/${VERSION}/g" common/constants.go
 
 mkdir -p out/linux
-go build -v -i -o out/linux ./...
+go build -ldflags "-s -w" -v -i -o out/linux ./...
 
 # Revert the version back to normal
 sed -i "s/${VERSION}/__IMAGE_SORTER_VERSION__/g" common/constants.go
--- script/build-win.sh
+++ script/build-win.sh
@@ -5,7 +5,7 @@ VERSION="$(cat VERSION)-$(git rev-parse --short HEAD)"
 sed -i "s/__IMAGE_SORTER_VERSION__/${VERSION}/g" common/constants.go
 
 mkdir -p out/windows
-go build -v -i -o out/windows ./...
+go build -ldflags "-s -w" -v -i -o out/windows ./...
 
 # Revert the version back to normal
 sed -i "s/${VERSION}/__IMAGE_SORTER_VERSION__/g" common/constants.go
