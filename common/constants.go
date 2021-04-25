e48fa4dd759e7e6277fb7ba8c818ff0547a31ac9
Juha Siponen <juha.siponen@vincit.fi>
2021-04-25 20:45:38 +0300

    Update copyright year

--- common/constants.go
+++ common/constants.go
@@ -3,5 +3,5 @@ package common
 const AppName = "Image Sorter"
 const Version = "__IMAGE_SORTER_VERSION__"
 const Author = "Juha Siponen"
-const CopyrightYear = "2020"
+const CopyrightYear = "2020 - 2021"
 const WebSiteUrl = "https://github.com/juhasipo/image-sorter"
